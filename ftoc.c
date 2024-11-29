331b3f7d9554bddabffdb0ee8f0cf0ad6d846a49
CodeButcher <Satyasrinip@gmail.com>
2024-11-30 04:41:51 +0530

    Revert "Add files via upload"
    This reverts commit c9633fba8bcc70c00a9d9f06e44ba078f3ab90a0.

--- ftoc.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main(){
-float F,C;
-printf("Enter in C: ");
-scanf("%f",&C);
-F=9*C/5+(32);
-printf("F is %f\n",F);
-}
-
--- markscard.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main(){
-printf("\t \t \t Marks card \t \t \t \n ");
-printf("\t SUBJECT: \t \t Marks: \t \t \n");
-printf("\t PHYSICS: \t \t 90 \t \t \n");
-printf("\t MATHS: \t \t 97\t \t \n");
-printf("\t CHEMISTRY: \t \t 87 \t \t \n");
-printf("\t TOTAL: \t \t  273 \t \t \n");
-}
